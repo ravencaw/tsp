2c287488331ef3868e5af2d62c3bd65bf34e1593
ravencaw <sebastianpedrosa.iespe@gmail.com>
2018-12-14 13:25:23 +0100

    Add files via upload

---
+++ src/tsp/TSP.java
@@ -0,0 +1,282 @@
+package tsp;
+
+import java.io.*;
+import java.util.*;
+
+
+public class TSP {
+	
+	public static int[] generaPrimera(int numCiudades) {
+        int[] sol = new int[numCiudades];
+        for (int i = 0; i < numCiudades; i++) {
+            sol[i] = i;
+        }
+        return sol;
+    }
+	
+	public static double fuerzaBruta(int[] ciudades, int i, int n, double mejor, double[][] dist) {
+        int j;
+        double aux;
+        if (i == n) {
+            aux = getCoste(ciudades, dist);
+            if (aux < mejor) {
+                System.out.println("");
+                for (int k = 0; k < ciudades.length; k++) {
+                    System.out.print(ciudades[k] + ",");
+                    if (k % 10 == 0 && k != 0) {
+                        System.out.println("");
+                    }
+                }
+                mejor = aux;
+            }
+        } else {
+            for (j = i; j < n; j++) {
+                swap(ciudades, i, j);
+                mejor = fuerzaBruta(ciudades, i + 1, n, mejor, dist);
+            }
+        }
+        return mejor;
+    }
+	
+	 public static int[] swap(int[] array, int x, int y) {
+	        int aux = array[x];
+	        array[x] = array[y];
+	        array[y] = aux;
+	        return array;
+	    }
+    
+    public static double getCoste(int[] ciudad, double[][] distancia) {
+    	double coste = 0;
+    	
+    	for (int i = 0; i < ciudad.length-1; i++) {
+			int ii=ciudad[i];
+			int jj=ciudad[i+1];
+			coste+=distancia[ii][jj];
+		}
+    	
+    	coste+=distancia[ciudad.length-1][0];
+    	
+    	return coste;
+    }
+    
+    public static int[] generaSolucion(int n) {
+        int[] s1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            s1[i] = i;
+        }
+        Random rand = new Random();
+
+        for (int i = 0; i < n; i++) {
+            int a = rand.nextInt(n);
+            int b = rand.nextInt(n);
+            int aux = s1[a];
+            s1[a] = s1[b];
+            s1[b] = aux;
+        }
+        return s1;
+    }
+
+    public static int[] busquedaAleatoriaConIteracionesFijas(int nCiudades, double[][] distancia, int busquedas) {
+
+        int[] s1 = new int[nCiudades];
+        int[] s2 = new int[nCiudades];
+        for (int i = 0; i < nCiudades; i++) {
+            s1[i] = i;
+        }
+
+        for (int i = 0; i < busquedas; i++) {
+            s2 = generaSolucion(nCiudades);
+            double a = getCoste(s1, distancia);
+            double b = getCoste(s2, distancia);
+            if (b < a) {
+                for (int j = 0; j < nCiudades; j++) {
+                    s1[j] = s2[j];
+                }
+            }
+        }
+        return s1;
+    }
+
+    public static int[] busquedaAleatoriaIteracionesReiniciables(int nCiudades, double[][] distancia, int busquedas) {
+        int[] s1 = new int[nCiudades];
+        int[] s2 = new int[nCiudades];
+        for (int i = 0; i < nCiudades; i++) {
+            s1[i] = i;
+        }
+        int b = 0;
+        while (b < busquedas) {
+            s2 = generaSolucion(nCiudades);
+            if (getCoste(s2, distancia) < getCoste(s1, distancia)) {
+                for (int i = 0; i < nCiudades; i++) {
+                    s1[i] = s2[i];
+                }
+                b = 0;
+            } else {
+                b++;
+            }
+        }
+        return s1;
+    }
+    
+    private static double[][] leerFichero(String nombreFichero) {
+        double[][] distancia = null;
+        double[][] ciudades = null;
+        int nCiudades = 0;
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader(nombreFichero);
+            br = new BufferedReader(fr);
+
+            String lineaActual;
+
+            br = new BufferedReader(new FileReader(nombreFichero));
+            lineaActual = br.readLine();
+            while (lineaActual != null) {
+
+                if (lineaActual.startsWith("DIMENSION")) {
+                    String[] lineaDividida2 = lineaActual.split(" ");
+                    nCiudades = Integer.parseInt(lineaDividida2[lineaDividida2.length - 1]);
+                    System.out.println(lineaActual);
+                    ciudades = new double[nCiudades][2];
+                    distancia = new double[nCiudades][nCiudades];
+
+                } else if (lineaActual.startsWith("NODE_COORD_SECTION")) {
+                    String[] lineaDividida2;
+                    int i = 0;
+                    lineaActual = br.readLine();
+                    while (lineaActual != null && i < nCiudades) {
+                        int a = 0;
+                        lineaDividida2 = lineaActual.split(" ");
+
+                        while (!lineaContieneCoordenada(lineaDividida2[a])) {
+                            a++;
+                        }
+                        int ciudad = Integer.parseInt(lineaDividida2[a]);
+                        a++;
+
+                        while (!lineaContieneCoordenada(lineaDividida2[a])) {
+                            a++;
+                        }
+                        double x = Double.parseDouble(lineaDividida2[a]);
+                        a++;
+
+                        while (!lineaContieneCoordenada(lineaDividida2[a])) {
+                            a++;
+                        }
+                        double y = Double.parseDouble(lineaDividida2[a]);
+                        
+
+                        ciudades[ciudad - 1][0] = x;
+                        ciudades[ciudad - 1][1] = y;
+
+                        i++;
+                        lineaActual = br.readLine();
+                    }
+                } else if (lineaActual != null) {
+                    System.out.println(lineaActual);
+                }
+                lineaActual = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        for (int i = 0; i < nCiudades; i++) {
+            distancia[i][i] = 0;
+            for (int j = i + 1; j < nCiudades; j++) {
+                distancia[i][j] = distancia[j][i] = Math.sqrt(
+                        (ciudades[i][0] - ciudades[j][0]) * (ciudades[i][0] - ciudades[j][0])
+                        + (ciudades[i][1] - ciudades[j][1]) * (ciudades[i][1] - ciudades[j][1]));
+            }
+        }
+        return distancia;
+    }
+    
+    
+    private static boolean lineaContieneCoordenada(String lineaActual) {
+        boolean equ = false;
+        if (lineaActual.startsWith("1") || lineaActual.startsWith("2")
+                || lineaActual.startsWith("3") || lineaActual.startsWith("4")
+                || lineaActual.startsWith("5") || lineaActual.startsWith("6")
+                || lineaActual.startsWith("7") || lineaActual.startsWith("8")
+                || lineaActual.startsWith("9")) {
+            equ = true;
+        }
+        return equ;
+    }
+
+ 
+    public static void main(String[] args) {
+    	
+    	System.out.println("\nEXPERIMENTO DEL TSP\n\n");
+        int iteraciones = 100;
+        String[] instancias = {"berlin52.tsp", "kroA100.tsp", "kroA150.tsp", "kroA200.tsp",
+                "a280.tsp", "vm1084.tsp", "vm1748.tsp", "usa13509.tsp"};
+
+        double[][] duraciones = new double[instancias.length][3];
+
+        for (int i = 0; i < instancias.length; i++) {
+            System.out.println("\n\n--------------------------------------------");
+            System.out.println("Instancia " + (i + 1));
+            double[][] distancia = leerFichero("data\\" + instancias[i]);
+            int nCiudades = distancia.length;
+            double s = 0;
+            int[] s1 = new int[nCiudades];
+            int repeticiones = 50;
+            
+
+            System.out.println("\nEjecutando Fuerza bruta");
+            double start = System.currentTimeMillis();
+            	s = fuerzaBruta(s1, 0, 9, getCoste(s1, distancia), distancia);
+            double stop = System.currentTimeMillis();
+            duraciones[i][0] = (stop - start);
+
+            System.out.println("Ejecutando Búsqueda aleatoria iteraciones fijas");
+            start = System.currentTimeMillis();
+            for (int j = 0; j < repeticiones; j++) {
+                s1 = busquedaAleatoriaConIteracionesFijas(nCiudades, distancia, iteraciones);
+            }
+            stop = System.currentTimeMillis();
+            duraciones[i][1] = (stop - start) / repeticiones;
+
+            System.out.println("Ejecutando Búsqueda aleatoria iteraciones reiniciables");
+            start = System.currentTimeMillis();
+            for (int j = 0; j < repeticiones; j++) {
+                s1 = busquedaAleatoriaIteracionesReiniciables(nCiudades, distancia, iteraciones);
+            }
+            stop = System.currentTimeMillis();
+            duraciones[i][2] = (stop - start) / repeticiones;
+            System.out.println("--------------------------------------------");
+            
+        }
+
+        System.out.println("\n\n\n--------------------------------------------");
+        System.out.println("Resultados finales");
+        for (int i1 = 0; i1 < instancias.length; i1++) {
+            System.out.println("Instancia " + (i1 + 1));
+            System.out.println("\tFB: " + duraciones[i1][0]+" ms");     //Fuerza bruta
+            System.out.println("\tBAIF: " + duraciones[i1][1]+" ms");  //Búsqueda aleatoria con iteraciones fijas
+            System.out.println("\tBAIR: " + duraciones[i1][2]+" ms");  //Búsqueda aleatoria con iteraciones reiniciables
+        }
+    
+
+        
+    }
+} 
+    
+    
+    
---
+++ src/tsp/TSP_2.java
@@ -0,0 +1,485 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tsp;
+
+/**
+ *
+ * @author Ander
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class TSP_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nEXPERIMENTO DEL TSP\n\n");
+        int iteraciones = 100;
+        Random rnd = new Random();
+        String[] instancias = {"a280.tsp", "berlin52.tsp", "kroA100.tsp", "kroA150.tsp", "kroA200.tsp",
+            "usa13509.tsp", "vm1084.tsp", "vm1748.tsp"};
+
+        double[][] duraciones = new double[instancias.length][3];
+
+        for (int i = 0; i < instancias.length; i++) {
+            System.out.println("\n\n--------------------------------------------");
+            System.out.println("Instancia " + (i + 1));
+            double[][] dist = leerFichero("data\\" + instancias[i]);
+            int nciudades = dist.length;
+            int[] s1 = new int[nciudades];
+            int repeticiones = 500;
+
+            System.out.println("Ejecutando Bactracking");
+            double start = System.currentTimeMillis();
+            for (int j = 0; j < repeticiones; j++) {
+                //Backtracking(dist, nciudades);
+            }
+            double stop = System.currentTimeMillis();
+            duraciones[i][0] = (stop - start) / repeticiones;
+
+            System.out.println("Ejecutando Búsqueda Local");
+            start = System.currentTimeMillis();
+            for (int j = 0; j < repeticiones; j++) {
+                s1 = TSPLocalSearch(3, nciudades, 1432, dist, GreedyAlgorithm(dist, 0));
+            }
+            stop = System.currentTimeMillis();
+            duraciones[i][1] = (stop - start) / repeticiones;
+
+            System.out.println("Ejecutando Divide y Vencerás");
+            start = System.currentTimeMillis();
+            for (int j = 0; j < repeticiones; j++) {
+                solve(dist, 0);
+            }
+            stop = System.currentTimeMillis();
+            duraciones[i][2] = (stop - start) / repeticiones;
+            System.out.println("--------------------------------------------");
+        }
+
+        System.out.println("\n\n\n--------------------------------------------");
+        System.out.println("Resultados finales");
+        for (int i = 0; i < instancias.length; i++) {
+            System.out.println("\nInstancia " + (i + 1));
+            System.out.println("\tBT: " + duraciones[i][0]);    //BACTRACKING
+            System.out.println("\tBL: " + duraciones[i][1]);    //BUSQUEDA LOCAL
+            System.out.println("\tDV: " + duraciones[i][2]);    //DIVIDE Y VENCERAS
+        }
+    }
+
+    public static void Backtracking(double[][] dist, int nciudades) throws Exception {
+
+        int[] solucion = creaMatriz(nciudades);
+        int[] recorrido = creaMatriz(nciudades);
+
+        permute(recorrido, dist, solucion);
+
+        System.out.print("\nMejor solucion:");
+        for (int i = 0; i < nciudades; i++) {
+            System.out.print(" " + solucion[i]);
+        }
+        System.out.println(" Distancia --------> " + evaluate(solucion, dist));
+    }
+
+    static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    static void permute(int[] arr, double[][] dist, int[] solucion) {
+        permute(arr, 0, arr.length - 1, dist, solucion);
+    }
+
+    static void permute(int[] arr, int i, int n, double[][] dist, int[] solucion) {
+        int j;
+        if (i == n) {
+            if (evaluate(arr, dist) < evaluate(solucion, dist)) { //Si es mejor la guardamos
+                for (int x = 0; x <= arr.length - 1; x++) {
+                    solucion[x] = arr[x];
+                }
+            }
+        } else {
+            for (j = i; j <= n; j++) {
+                swap(arr, i, j);
+                if (i != 0) {
+                    int[] arrayAuxiliar = rellenarAuxiliar(i, arr); //creamos un array auxiliar que va a tener desde 0 hasta el elemento que hemos permutado, si al evaluar este array la distancia es mayor que la solucion podamos este camino
+                    if (evaluateAuxiliar(arrayAuxiliar, dist) < evaluate(solucion, dist)) {   //Si el camino es prometedor seguimos si no no
+                        permute(arr, i + 1, n, dist, solucion); //nos adentramos un nivel mas, puede resultar que al final sea mas largo pero en el paso anterior no lo era
+                    }
+                    swap(arr, i, j);    //deshacemos el swap
+                } else {    //Fallaba en el 0 porque el arrayauxiliar no podia coger el primer elemento solo y decir si era menor, pero como siempre que sea principio de camino va a ser menor dejamos que se meta un nivel de recursividad
+                    permute(arr, i + 1, n, dist, solucion);
+                    swap(arr, i, j); // backtrack
+                }
+            }
+        }
+    }
+
+    //Búsqueda local
+    public static int[] TSPLocalSearch(int vecindad, int num_ciudades, int seed, double[][] dist, int[] solucion) { //Algoritmo que se encargara de buscar la mejor solucion posible
+
+        boolean fallo = false;
+        int[] auxiliar = creaMatriz(num_ciudades);  //para evitar los fallos de punteros al trabajar con la solucion
+        System.arraycopy(solucion, 0, auxiliar, 0, solucion.length);
+
+        while (!fallo && vecindad >= 0) {   //mientras no haya fallo y la vecindad sea mayor o igual a 0 (para la recursividad)
+            auxiliar = cambioCiudades(auxiliar, num_ciudades, seed);
+            if (evaluate(auxiliar, dist) < evaluate(solucion, dist)) {
+                for (int j = 0; j < num_ciudades; j++) { //Para copiar un array a otro
+                    solucion[j] = auxiliar[j];
+                }
+                System.arraycopy(TSPLocalSearch(vecindad - 1, num_ciudades, seed, dist, solucion), 0, solucion, 0, solucion.length);    //copiamos la solucion al meternos recursivamente
+            } else {
+                Random dado = new Random();
+                int tirada = dado.nextInt(100);
+
+                if (tirada < 50) {  //si la tirada
+                    System.arraycopy(TSPLocalSearch(vecindad - 1, num_ciudades, seed, dist, solucion), 0, auxiliar, 0, auxiliar.length);
+
+                    if (evaluate(auxiliar, dist) < evaluate(solucion, dist)) {
+                        for (int j = 0; j < num_ciudades; j++) { //Para copiar un array a otro
+                            solucion[j] = auxiliar[j];
+                        }
+                    }
+                } else {    //Salimos del nivel de recursividad si falla la tirada
+                    fallo = true;
+                }
+            }
+            vecindad--;
+        }
+        return solucion;
+    }
+
+    public static int[] GreedyAlgorithm(double[][] distances, int initialPosition) {//es la solucion
+        //Genera un array solución para introducir los datos de nuestra solucion
+        int[] route = new int[distances.length];
+        int numCities = distances.length;
+
+        int i = 1;
+        int current = initialPosition;
+        int[] citiesAvailable = new int[numCities];
+        //Arrays.fill(citiesAvailable, 0);
+        route[i] = current;
+        int max = route[i];
+        while (i < numCities - 1) {
+            //Introducimos un 1 en cada ciudad visitada y con el método 
+            //NextCity comprobamos las ciudades más cercanas para obtener 
+            //la distancia de esta proxima
+            citiesAvailable[current] = 1;
+            int next = TSP_2.nextCity(citiesAvailable, current, distances);
+            route[i] = next;
+            current = next;
+            i++;
+        }
+
+        for (int j = 0; j < route.length; j++) {
+            int num = route[i];
+            if (num < max) {
+                max = route[i];
+            }
+
+        }
+        return route;
+    }
+
+    //Divide y vencerás
+    public static int[] solve(double[][] distances, int initialPosition) {
+        int[] route = new int[distances.length];
+        int numCities = distances.length;
+
+        int i = 1;
+        int current = initialPosition;
+        int[] citiesAvailable = new int[numCities];
+        Arrays.fill(citiesAvailable, 0);
+        route[0] = current;
+
+        while (i < numCities) {
+            citiesAvailable[current] = 1;
+            int next = nextCity(citiesAvailable, current, distances, i);
+            route[i] = next;
+            current = next;
+            i++;
+        }
+        return route;
+    }
+
+    private static int nextCity(int[] cavailable, int current, double[][] distances, int cRecorridas) {
+        int nc = -1, j = 0;
+        double path = Double.MAX_VALUE;
+        double[][] dCiudad = new double[cavailable.length - cRecorridas][2], dOrdenado;
+
+        for (int i = 0; i < cavailable.length; i++) {
+            if (cavailable[i] == 0) {
+                if (i < current) {
+
+                    dCiudad[j][0] = i;
+                    dCiudad[j][1] = distances[i][current - i];
+                    j++;
+
+                } else if (i >= current) {
+                    dCiudad[j][0] = i;
+                    dCiudad[j][1] = distances[current][i - current];
+                    j++;
+                }
+            }
+        }
+
+        dOrdenado = divideYVenceras(dCiudad);
+
+        nc = (int) dOrdenado[0][0];
+        cavailable[nc] = 1;
+        return nc;
+    }
+
+    public static double[][] divideYVenceras(double[][] entrada) {
+        double[][][] subS = new double[2][][], subP;
+        double[][] s;
+        if (entrada.length == 1) {
+            s = entrada;
+        } else {
+            subP = divide(entrada);
+            for (int i = 0; i < 2; i++) {
+                subS[i] = divideYVenceras(subP[i]);
+            }
+            s = combina(subS, entrada.length);
+        }
+        return s;
+    }
+
+    //Funciones auxiliares
+    public static double[][][] divide(double[][] entrada) {
+        double[][][] s = new double[2][entrada.length / 2][2];
+        int j = 0, k = 0;
+        if (entrada.length % 2 == 1) {
+            s[1] = new double[entrada.length / 2 + 1][2];
+        }
+        for (int i = 0; i < entrada.length; i++) {
+            if (i < (entrada.length / 2)) {
+                s[0][j] = entrada[i];
+                j++;
+            } else {
+                s[1][k] = entrada[i];
+                k++;
+            }
+        }
+        return s;
+    }
+
+    public static double[][] combina(double[][][] entrada, int k) {
+        double[][] s = new double[k][2];
+        int j = 0, l = 0;
+        for (int i = 0; i < entrada[0].length; i++) {
+            while (entrada[1].length > j && entrada[0][i][1] > entrada[1][j][1]) {
+                s[l] = entrada[1][j];
+                j++;
+                l++;
+            }
+            s[l] = entrada[0][i];
+            l++;
+        }
+        if (l < k) {
+            while (entrada[1].length > j) {
+                s[l] = entrada[1][j];
+                j++;
+                l++;
+            }
+        }
+        return s;
+    }
+
+    public static double pathCost(int[] path, double[][] dist) {
+        double w = 0;
+
+        for (int i = 0; i < path.length - 1; i++) {
+            if (path[i + 1] < path[i]) {
+                w += dist[path[i + 1]][path[i] - path[i + 1]];
+            } else {
+                w += dist[path[i]][path[i + 1] - path[i]];
+            }
+        }
+        return w;
+    }
+
+    private static int nextCity(int[] cavailable, int current, double[][] distances) {
+        int nc = -1;
+        double path = Double.MAX_VALUE;
+
+        for (int i = 0; i < cavailable.length; i++) {
+            if (i < current) {
+                if (cavailable[i] == 0 && distances[i][current - i] < path) {
+                    nc = i;
+                    path = distances[i][current - i];
+                }
+            } else if (i >= current) {
+                if (cavailable[i] == 0 && distances[current][i - current] < path) {
+                    nc = i;
+                    path = distances[current][i - current];
+                }
+            }
+        }
+        cavailable[nc] = 1;
+        return nc;
+    }
+
+    public static int[] rellenarAuxiliar(int j, int[] arr) {
+        int[] aleatorio = creaMatriz(j);
+        for (int i = 0; i < j; i++) {
+            aleatorio[i] = arr[i];
+        }
+        return aleatorio;
+    }
+
+    public static double evaluate(int[] cities, double[][] dist) { //Esta funcion te va a dar el coste de la ruta
+        double cost = 0.;
+
+        for (int i = 0; i < cities.length - 1; i++) {
+            int ii = cities[i];
+            int jj = cities[i + 1];
+            cost += dist[ii][jj];
+        }
+
+        cost += dist[cities.length - 1][0];
+
+        return cost;
+    }
+
+    public static double evaluateAuxiliar(int[] cities, double[][] dist) { //Esta funcion te va a dar el coste de la ruta
+        double cost = 0.;
+
+        for (int i = 0; i < cities.length - 1; i++) {
+            int ii = cities[i];
+            int jj = cities[i + 1];
+            cost += dist[ii][jj];
+        }
+        return cost;
+    }
+
+    public static int[] cambioCiudades(int[] recorrido, int n, int seed) { //Esta funcion va a generar un camino nuevo aleatorio generando dos numeros aleatorios y cambiando sus indices entre ellos
+
+        Random rnd = new Random();
+        Random rna = new Random();
+        int indice1 = rnd.nextInt(n);
+        int indice2 = rna.nextInt(n);
+        int aux = 0;
+
+        aux = recorrido[indice1];
+        recorrido[indice1] = recorrido[indice2];
+        recorrido[indice2] = aux;
+
+        return recorrido;
+    }
+
+    public static int[] creaMatriz(int nc) { //Metodo para crear una matriz 
+        int[] A = new int[nc];
+
+        for (int i = 0; i < A.length; i++) {
+            A[i] = i;
+        }
+        return A;
+    }
+
+    private static boolean lineaContieneCoordenada(String lineaActual) {
+        boolean equ = false;
+        if (lineaActual.startsWith("1") || lineaActual.startsWith("2")
+                || lineaActual.startsWith("3") || lineaActual.startsWith("4")
+                || lineaActual.startsWith("5") || lineaActual.startsWith("6")
+                || lineaActual.startsWith("7") || lineaActual.startsWith("8")
+                || lineaActual.startsWith("9")) {
+            equ = true;
+        }
+        return equ;
+    }
+    
+
+    private static double[][] leerFichero(String FILENAME) {
+        double[][] dist = null;
+        double[][] ciudades = null;
+        int nciudades = 0;
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader(FILENAME);
+            br = new BufferedReader(fr);
+
+            String lineaActual;
+
+            br = new BufferedReader(new FileReader(FILENAME));
+            lineaActual = br.readLine();
+            while (lineaActual != null) {
+
+                if (lineaActual.startsWith("DIMENSION")) {
+                    String[] lineaDividida2 = lineaActual.split(" ");
+                    nciudades = Integer.parseInt(lineaDividida2[lineaDividida2.length - 1]);
+                    ciudades = new double[nciudades][2];
+                    dist = new double[nciudades][nciudades];
+
+                } else if (lineaActual.startsWith("NODE_COORD_SECTION")) {
+                    String[] lineaDividida2;
+                    int i = 0;
+                    lineaActual = br.readLine();
+                    while (lineaActual != null && i < nciudades) {
+                        int a = 0;
+                        lineaDividida2 = lineaActual.split(" ");
+
+                        while (!lineaContieneCoordenada(lineaDividida2[a])) {
+                            a++;
+                        }
+                        int ciudad = Integer.parseInt(lineaDividida2[a]);
+                        a++;
+
+                        while (!lineaContieneCoordenada(lineaDividida2[a])) {
+                            a++;
+                        }
+                        double x = Double.parseDouble(lineaDividida2[a]);
+                        a++;
+
+                        while (!lineaContieneCoordenada(lineaDividida2[a])) {
+                            a++;
+                        }
+                        double y = Double.parseDouble(lineaDividida2[a]);
+
+                        ciudades[ciudad - 1][0] = x;
+                        ciudades[ciudad - 1][1] = y;
+
+                        i++;
+                        lineaActual = br.readLine();
+                    }
+                } else if (lineaActual != null) {
+                    System.out.println(lineaActual);
+                }
+                lineaActual = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < nciudades; i++) {
+            dist[i][i] = 0;
+            for (int j = i + 1; j < nciudades; j++) {
+                dist[i][j] = dist[j][i] = Math.sqrt(
+                        (ciudades[i][0] - ciudades[j][0]) * (ciudades[i][0] - ciudades[j][0])
+                        + (ciudades[i][1] - ciudades[j][1]) * (ciudades[i][1] - ciudades[j][1]));
+            }
+        }
+        return dist;
+    }
+}
